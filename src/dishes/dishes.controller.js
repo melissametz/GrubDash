2cd345df028a758612c2d963d684434aa5ed9042
Melissa Metz <110804486+melissametz@users.noreply.github.com>
2022-11-30 10:22:13 -0800

    Second Commit

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -12,107 +12,118 @@ read, update, and list dishes. Note that dishes cannot be deleted*/
 
 //list dishes
 const list = (req, res) => {
-    res.json({data: dishes});
+  res.json({ data: dishes });
 };
 
 //validates id - must include id
 const idValidator = (req, res, next) => {
-    const {data: {id} = {}} = req.body;
-    if (id === undefined ||  id === null || id.length===0) {
-        return next();
-    };
-    if (id && id === req.params.dishId) {
-        return next();
-    };
-    next({status: 400, message: `id does not match ${id}`});
+  const { data: { id } = {} } = req.body;
+  if (id === undefined || id === null || id.length === 0) {
+    return next();
+  }
+  if (id && id === req.params.dishId) {
+    return next();
+  }
+  next({ status: 400, message: `id does not match ${id}` });
 };
 
 //validates name - must include name
 const nameValidator = (req, res, next) => {
-    const {data: {name} = {} } = req.body;
-    if (name) {
-        res.locals.name = name;
-        return next();
-    };
-    next({status: 400, message: "A 'name' property is required."});
+  const { data: { name } = {} } = req.body;
+  if (name) {
+    res.locals.name = name;
+    return next();
+  }
+  next({ status: 400, message: "A 'name' property is required." });
 };
 
 //validates description - must include description
 const descValidator = (req, res, next) => {
-    const {data: {description} = {} } = req.body;
-    if (description) {
-        res.locals.desc = description;
-        return next();
-    };
-    next({status: 400, message: "A 'description' property is required."});
+  const { data: { description } = {} } = req.body;
+  if (description) {
+    res.locals.desc = description;
+    return next();
+  }
+  next({ status: 400, message: "A 'description' property is required." });
 };
 
 //validates price - must include price
 const priceValidator = (req, res, next) => {
-    const {data: {price} = {} } = req.body;
-    if (price > 0 && typeof(price)==='number') {
-        res.locals.price = price;
-        return next();
-    };
-    next({status: 400, message: "A 'price' property is required."});
+  const { data: { price } = {} } = req.body;
+  if (price > 0 && typeof price === "number") {
+    res.locals.price = price;
+    return next();
+  }
+  next({ status: 400, message: "A 'price' property is required." });
 };
 
 //validates img_url - must include img_url
 const imgValidator = (req, res, next) => {
-    const {data: {image_url} = {} } = req.body;
-    if (image_url) {
-        res.locals.imgurl = image_url;
-        return next();
-    };
-    next({status: 400, message: "A 'image_url' property is required."});
+  const { data: { image_url } = {} } = req.body;
+  if (image_url) {
+    res.locals.imgurl = image_url;
+    return next();
+  }
+  next({ status: 400, message: "A 'image_url' property is required." });
 };
 
 //create new dish
 const create = (req, res) => {
-    const newDish = {
-        id: nextId(),
-        name: res.locals.name,
-        description: res.locals.desc,
-        price: res.locals.price,
-        image_url: res.locals.imgurl
-    };
-    dishes.push(newDish);
-    res.status(201).json({data: newDish});
+  const newDish = {
+    id: nextId(),
+    name: res.locals.name,
+    description: res.locals.desc,
+    price: res.locals.price,
+    image_url: res.locals.imgurl,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
 };
 
 //Checks dish exists
 const dishExists = (req, res, next) => {
-    const dishId = req.params.dishId;
-    const foundDish = dishes.find((dish) => dish.id === dishId);
-    if (foundDish) {
-        res.locals.dishId = dishId;
-        res.locals.dish = foundDish;
-        return next();
-    };
-    next({status: 404, message: `Dish id not found ${res.locals.dish}`});
+  const dishId = req.params.dishId;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
+    res.locals.dishId = dishId;
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({ status: 404, message: `Dish id not found ${res.locals.dish}` });
 };
 
 //reads dishes
 const read = (req, res) => {
-    res.json({data: res.locals.dish});
+  res.json({ data: res.locals.dish });
 };
 
 //update dishes
 const update = (req, res) => {
-    res.locals.dish = {
-        id: res.locals.dishId,
-        name: res.locals.name,
-        description: res.locals.desc,
-        price: res.locals.price,
-        image_url: res.locals.imgurl
-    };
-    res.json({data: res.locals.dish});
+  const {
+    data: { name, description, price, image_url/*, id*/ },
+  } = req.body;
+
+  //res.locals.dish.id = id;
+  res.locals.dish.name = name;
+  res.locals.dish.description = description;
+  res.locals.dish.price = price;
+  res.locals.dish.image_url = image_url;
+
+  res.json({ data: res.locals.dish });
 };
 
 //exports functions for use by router
 module.exports = {
-    list,
-    create: [nameValidator, descValidator, priceValidator, imgValidator, create],
-    read: [dishExists, read],
-    update: [dishExists, nameValidator, descValidator, priceValidator, imgValidator, idValidator, update]
+  list,
+  create: [nameValidator, descValidator, priceValidator, imgValidator, create],
+  read: [dishExists, read],
+  update: [
+    dishExists,
+    nameValidator,
+    descValidator,
+    priceValidator,
+    imgValidator,
+    idValidator,
+    update,
+  ],
 };
